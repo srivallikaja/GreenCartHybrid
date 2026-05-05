8de248f6f2374b5c9071d7b2dce5c7608dd4f4e1
srivallikaja <sravani.kaja91@gmail.com>
2026-05-05 16:16:35 -0600

    Add ChromeOptions import to BaseClass

--- src/test/java/utils/BaseClass.java
+++ src/test/java/utils/BaseClass.java
@@ -1,7 +1,7 @@
 package utils;
 
 import java.time.Duration;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
